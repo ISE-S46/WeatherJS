e7cefba0a43fdea1b19a2c81d5261173eb870b9f
ISE <ise.sns46@gmail.com>
2025-05-06 18:13:54 +0700

    Add Bootstrap and search city function

--- ScriptMain.js
+++ ScriptMain.js
@@ -1,14 +1,55 @@
 import { API_key } from "./config.js";
 
-const lat = 13.736717;
-const lon = 100.523186;
 const key = API_key;
 
-const API = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key;
+const Location = document.getElementById("InputLocation");
+const submit = document.getElementById("submit");
 
-console.log(API);
+let LocationName;
 
-async function GetWeather() {
+submit.addEventListener("click", GetLocationName)
+Location.addEventListener("keydown", (key) => {
+    if (key.key === "Enter") {
+        GetLocationName();
+    }
+})
+
+function GetLocationName() {
+    LocationName = Location.value;
+    const LatLongAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${LocationName}&appid=${API_key}`;
+    // console.log(LatLongAPI);
+    GetLatLong(LatLongAPI);
+}
+
+async function GetLatLong(LatLongAPI) {
+    try{
+        const Response = await fetch(LatLongAPI);
+
+        if (!Response.ok) {
+            alert("Please Enter Valid City Name");
+            throw new Error("Respond error: " + Response.statusText);
+        }
+        const LatLonData = await Response.json();
+        let lat = LatLonData[0].lat;
+        let lon = LatLonData[0].lon;
+
+        // console.log(lat);
+        // console.log(lon);
+
+        CurrentWeather(lat, lon);
+
+    }catch (error){
+        console.error('GetLatLong() Error:', error);
+    }
+}
+
+function CurrentWeather(lat, lon) {
+    const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+    console.log(API);
+    GetWeather(API)
+}
+
+async function GetWeather(API) {
     try {
         const Response = await fetch(API);
 
@@ -17,13 +58,18 @@ async function GetWeather() {
         }
 
         const WeatherData = await Response.json();
-        
+
         console.log(WeatherData);
         const WeatherContainer = document.getElementById("Weather");
+
+        const WeatherDiv = document.createElement("div");
+        WeatherDiv.innerHTML = `
+            <p>${WeatherData.name}</p>
+        `;
+
+        WeatherContainer.appendChild(WeatherDiv);
 
     } catch (error) {
         console.error('GetWeather() Error:', error);
     }
 }
-
-GetWeather();
